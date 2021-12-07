f26d00303f126e0bbaf9befbea6874b273f1a07c
hyeongwoo94 <87638160+hyeongwoo94@users.noreply.github.com>
2021-12-07 19:44:23 +0900

    초기화

--- src/App.js
+++ src/App.js
@@ -1,42 +1,9 @@
+import { reset } from "ansi-colors";
 import { useState } from "react";
 const App = () => {
-  const [minutes, setMinutes]= useState();
-  const onChange = (event) => {
-    setMinutes(event.target.value);
-  };
-  const reset = () => {
-    setMinutes(0)
-  }
-  const [flipped, setFlipped] = useState(false);
-  const onFlip= () => {
-    reset();
-    setFlipped((current)=> !current)
-  }
   return(
     <>
-    <h1 style={{textAlign:"center"}}>단위 변환기</h1>
-    <div>
-      <label htmlFor="minutes" style={{marginRight:"5px"}}>분</label>
-      <input 
-      id="minutes" 
-      placeholder="분" 
-      type="number" 
-      value={flipped ? Math.round(minutes * 60) : minutes }
-      onChange={onChange}
-      disabled={flipped === true}
-      />
-      <button onClick={reset} style={{marginLeft:"5px"}}>리셋</button>
-      <br></br>
-      <label htmlFor="hours" style={{marginRight:"5px"}}>시</label>
-      <input id="hours"
-      placeholder="시간"
-      type="number" 
-      value={flipped ? minutes : Math.round(minutes / 60)}
-      onChange={onChange}
-      disabled={flipped === false}
-      />
-      <button onClick={onFlip}>변환</button>
-    </div>
+    ...
     </>
   )
 }
