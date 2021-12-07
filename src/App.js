c0a2e7a7184711b31b9f99ed252e8b3fd0aad017
hyeongwoo94 <87638160+hyeongwoo94@users.noreply.github.com>
2021-12-07 18:09:09 +0900

    input 새로운속성,리셋버튼

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ const App = () => {
   const onChange = (event) => {
     setMinutes(event.target.value);
   };
+  const reset = () => {
+    setMinutes(0)
+  }
   return(
     <>
     <h1 style={{textAlign:"center"}}>단위 변환기</h1>
-    <div style={{display:"grid", justifyContent:"space-around"}}>
-      <label htmlFor="minutes">분</label>
+    <div>
+      <label htmlFor="minutes" style={{marginRight:"5px"}}>분</label>
       <input 
       id="minutes" 
       placeholder="분" 
@@ -16,9 +19,15 @@ const App = () => {
       value={minutes}
       onChange={onChange}
       />
-      <h3>target{minutes}</h3>
-      <label htmlFor="hours">시</label>
-      <input if="hours" placeholder="시간" type="number" />
+      <button onClick={reset} style={{marginLeft:"5px"}}>리셋</button>
+      <br></br>
+      <label htmlFor="hours" style={{marginRight:"5px"}}>시</label>
+      <input id="hours"
+      placeholder="시간"
+      type="number" 
+      value={Math.round(minutes / 60)}
+      disabled
+      />
     </div>
     </>
   )
@@ -34,3 +43,10 @@ export default App;
 //  setMinutes(event.target.value);
 //};
 //이벤트 발생시 해당 타겟의 값을 가져온다.
+// value={Math.round(minutes / 60)} ===숫자를 반올림해준다.
+
+//const reset = () => {
+//  setMinutes(0)
+//}
+//state의 값을 0으로 돌아가니 리셋이다.
+//input 속성중 disabled은 입력을 못하게 막는 것이다.
