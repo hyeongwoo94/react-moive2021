e64a4932a54797d67b187eaff8edce7e2cf6dcd1
hyeongwoo94 <87638160+hyeongwoo94@users.noreply.github.com>
2021-12-07 17:20:22 +0900

    useState finish-v

--- src/App.js
+++ src/App.js
@@ -31,3 +31,4 @@ export default App;
 // }
 // **couter의 초기값을 useStat(초기값 설정) 후 setCouter(새로 리렌더링 될 함수 설정) 해주고 이것을 새 Click 함수를 만들고 boutton의 속성값으로 설정해준다
 // ===버튼 클릭할때 마다 couter가 증가한다 **  tip setCouter("짠" or 무언가) 이렇게 작성 후 클릭 한번했을 때 {couter}의 값이 "짠" or 무언가 이렇게 나온다.
+//current란 현재 값을 말한다.
