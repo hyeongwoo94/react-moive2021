f2fe6cdf4e736c4e5bcd8bca074aff6cfced9765
hyeongwoo94 <87638160+hyeongwoo94@users.noreply.github.com>
2021-12-07 17:49:08 +0900

    useState를 이용하여여input 활용

--- src/App.js
+++ src/App.js
@@ -1,34 +1,29 @@
 import { useState } from "react";
 const App = () => {
-  const [couter, setCounter] = useState(0);
-  const Click = () => {
-    setCounter(couter + 1);
-  }
+  const [minutes, setMinutes]= useState();
+  const onChange = (event) => {
+    setMinutes(event.target.value);
+  };
   return(
-    <div>
-      <h3> 클릭 횟수 : {couter}</h3>
-      <button onClick={Click}>클릭!</button>
+    <>
+    <h1 style={{textAlign:"center"}}>단위 변환기</h1>
+    <div style={{display:"grid", justifyContent:"space-around"}}>
+      <label htmlFor="minutes">분</label>
+      <input 
+      id="minutes" 
+      placeholder="분" 
+      type="number" 
+      value={minutes}
+      onChange={onChange}
+      />
+      <h3>target{minutes}</h3>
+      <label htmlFor="hours">시</label>
+      <input if="hours" placeholder="시간" type="number" />
     </div>
+    </>
   )
 }
 
 export default App;
 
 
-// #useState 사용법
-// const App = () => {
-//   const [couter, setCounter] = useState(0);
-//   const Click = () => {
-//     setCounter(couter + 1);
-//     ===setCounter((current) => current + 1); 위와 동일한 함수 하지만 좀더 안전하다. 현재 state를 가지고 새로운 값을 계산한다.
-//   };
-//   return(
-//     <div>
-//       <h3> 클릭 횟수 : {couter}</h3>
-//       <button onClick={Click}>클릭!</button>
-//     </div>
-//   )
-// }
-// **couter의 초기값을 useStat(초기값 설정) 후 setCouter(새로 리렌더링 될 함수 설정) 해주고 이것을 새 Click 함수를 만들고 boutton의 속성값으로 설정해준다
-// ===버튼 클릭할때 마다 couter가 증가한다 **  tip setCouter("짠" or 무언가) 이렇게 작성 후 클릭 한번했을 때 {couter}의 값이 "짠" or 무언가 이렇게 나온다.
-//current란 현재 값을 말한다.
