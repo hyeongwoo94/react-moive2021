68e4d80937740476f2e52845fc7a6e082e887f75
hyeongwoo94 <87638160+hyeongwoo94@users.noreply.github.com>
2021-12-07 19:07:25 +0900

    disabled를 넣어주고 반대로만들기

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ const App = () => {
   const reset = () => {
     setMinutes(0)
   }
+  const [flipped, setFlipped] = useState(false);
+  const onFlip= () => {
+    reset();
+    setFlipped((current)=> !current)
+  }
   return(
     <>
     <h1 style={{textAlign:"center"}}>단위 변환기</h1>
@@ -16,8 +21,9 @@ const App = () => {
       id="minutes" 
       placeholder="분" 
       type="number" 
-      value={minutes}
+      value={flipped ? Math.round(minutes * 60) : minutes }
       onChange={onChange}
+      disabled={flipped === true}
       />
       <button onClick={reset} style={{marginLeft:"5px"}}>리셋</button>
       <br></br>
@@ -25,9 +31,11 @@ const App = () => {
       <input id="hours"
       placeholder="시간"
       type="number" 
-      value={Math.round(minutes / 60)}
-      disabled
+      value={flipped ? minutes : Math.round(minutes / 60)}
+      onChange={onChange}
+      disabled={flipped === false}
       />
+      <button onClick={onFlip}>변환</button>
     </div>
     </>
   )
@@ -35,18 +43,12 @@ const App = () => {
 
 export default App;
 
-
-//useState는 기본적으로 array를 제공한다.const [minutes, setMinutes]
-//htmlFor를 사용하는 이유는 리액트는 jsx문번을 사용하기 때문에 몇가지가 더있다.
-//onChange == input에 변화가 생기면 리스닝해준다.
-//const onChange = (event) => {
-//  setMinutes(event.target.value);
-//};
-//이벤트 발생시 해당 타겟의 값을 가져온다.
-// value={Math.round(minutes / 60)} ===숫자를 반올림해준다.
-
-//const reset = () => {
-//  setMinutes(0)
-//}
-//state의 값을 0으로 돌아가니 리셋이다.
-//input 속성중 disabled은 입력을 못하게 막는 것이다.
+//setFlipped((current)=> !current)
+//이 함수 실행시 false가 true가 된다. !이란 반대를 말하는 것.
+//hours에도 onChang를 넣어준다.
+//value={flipped ? minutes : Math.round(minutes / 60)}
+//flipped가 참이라면 minutes 값이오고 flase라면 Math.round(minutes / 60)값이온다.
+//value={flipped ? Math.round(minutes * 60) : minutes }
+//분과 시간은 반대이기 때문에 반대로 적어줬다.
+//disabled의값이 분과 시간이 반대라는 것을 유념해야한다. disabled={flipped === false}
+//시간은 flase인게 참이고 분은 true인게 참이다.
