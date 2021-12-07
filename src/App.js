88ec7b73cdc08929e8737d2d5e8399c58d1d1d2c
hyeongwoo94 <87638160+hyeongwoo94@users.noreply.github.com>
2021-12-07 17:11:57 +0900

    useState사용법+설명

--- src/App.js
+++ src/App.js
@@ -13,3 +13,21 @@ const App = () => {
 }
 
 export default App;
+
+
+// #useState 사용법
+// const App = () => {
+//   const [couter, setCounter] = useState(0);
+//   const Click = () => {
+//     setCounter(couter + 1);
+//     ===setCounter((current) => current + 1); 위와 동일한 함수 하지만 좀더 안전하다. 현재 state를 가지고 새로운 값을 계산한다.
+//   };
+//   return(
+//     <div>
+//       <h3> 클릭 횟수 : {couter}</h3>
+//       <button onClick={Click}>클릭!</button>
+//     </div>
+//   )
+// }
+// **couter의 초기값을 useStat(초기값 설정) 후 setCouter(새로 리렌더링 될 함수 설정) 해주고 이것을 새 Click 함수를 만들고 boutton의 속성값으로 설정해준다
+// ===버튼 클릭할때 마다 couter가 증가한다 **  tip setCouter("짠" or 무언가) 이렇게 작성 후 클릭 한번했을 때 {couter}의 값이 "짠" or 무언가 이렇게 나온다.
